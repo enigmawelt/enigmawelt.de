fe78017230d5542a0dc6dda44bd1c5b980bb1f94
Gunnar Koser <34108544+enigmawelt@users.noreply.github.com>
2024-12-31 19:56:23 +0100

    v1.4.1 error corrected

--- src/plugin.py
+++ src/plugin.py
@@ -102,7 +102,7 @@ class enimaWeltScreen(Screen):
 		<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Enigmawelt/img/bg_fhd.png" position="0,871" size="1920,89" zPosition="-5"/>
 		<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Enigmawelt/img/logo.png" position="1720,11" size="68,68" alphatest="blend" scale="1"/>
 		<eLabel position="0,90" size="1800,780" backgroundColor="#024A4A" zPosition="-1"/>
-		<eLabel position="1630,25" size="100,40" text="v1.4" font="SRegular;24" foregroundColor="#FFFFFF" halign="center" valign="center" transparent="1"/>
+		<eLabel position="1630,25" size="100,40" text="v1.4.1" font="SRegular;24" foregroundColor="#FFFFFF" halign="center" valign="center" transparent="1"/>
 		<widget source="movielist" render="Listbox" position="18,108" size="1070,750" foregroundColor="#FFFFFF" foregroundColorSelected="#FFFFFF" backgroundColorSelected="#038181" scrollbarMode="showOnDemand" scrollbarWidth="6" scrollbarForegroundColor="#00C0C0" scrollbarBackgroundColor="#024A4A" transparent="1">
 			<convert type="TemplatedMultiContent">{"template": [ MultiContentEntryText(pos=(6,0), size=(1041,45), font=0, text=0, flags=RT_HALIGN_LEFT|RT_VALIGN_CENTER)], "fonts": [gFont("SRegular",33)], "itemHeight": 50 }</convert>
 		</widget>
@@ -130,7 +130,7 @@ class enimaWeltScreen(Screen):
 		<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Enigmawelt/img/bg_hd.png" position="0,581" size="1280,59" zPosition="-5"/>
 		<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Enigmawelt/img/logo.png" position="1146,7" size="45,45" alphatest="blend" scale="1"/>
 		<eLabel position="0,60" size="1200,520" backgroundColor="#024A4A" zPosition="-1"/>
-		<eLabel position="1080,18" size="75,20" text="v1.4" font="SRegular;17" foregroundColor="#FFFFFF" halign="center" valign="center" transparent="1"/>
+		<eLabel position="1080,18" size="75,20" text="v1.4.1" font="SRegular;17" foregroundColor="#FFFFFF" halign="center" valign="center" transparent="1"/>
 		<widget source="movielist" render="Listbox" position="12,72" size="713,500" foregroundColor="#FFFFFF" foregroundColorSelected="#FFFFFF" backgroundColorSelected="#038181" scrollbarMode="showOnDemand" scrollbarWidth="6" scrollbarForegroundColor="#00C0C0" scrollbarBackgroundColor="#024A4A" transparent="1">
 			<convert type="TemplatedMultiContent">{"template": [ MultiContentEntryText(pos=(4,0), size=(694,30), font=0, text=0, flags=RT_HALIGN_LEFT|RT_VALIGN_CENTER)], "fonts": [gFont("SRegular",22)], "itemHeight": 33 }</convert>
 		</widget>
